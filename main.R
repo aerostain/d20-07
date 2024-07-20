c1919d758583703cddc38fb8cafd081711e50162
Rick MarSal <richims026@gmail.com>
2024-07-20 10:05:16 -0500

    Map R _ok

--- main.R
+++ main.R
@@ -11,5 +11,22 @@ maps0 <- st_read(
 
 maps0 %>% str()
 
-maps0%>%
-group_by(SEDEOPERAT)%>%summarise(N=n())
+maps0 %>%
+  group_by(SEDEOPERAT) %>%
+  summarise(N = n())
+
+maps0f <-
+  maps0 %>%
+  filter(SEDEOPERAT %in%
+    c(
+      "LIMA", "ANCASH - CHIMBOTE",
+      "ANCASH - HUARAZ", "HUANUCO"
+    ))
+
+leaflet() %>%
+  addTiles() %>%
+  addPolygons(
+    data = maps0f,
+    popup = maps0f$SEDEOPERAT,
+    color = c("red", "green", "blue", "yellow")
+  )
